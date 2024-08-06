12dedc140a226c2dd20ced3d07c3b63387839d4c
david howard <dmh2000@gmail.com>
2024-08-06 16:58:15 -0700

    fix: Refactor CAN frame structure and update function signatures

--- can/can.h
+++ can/can.h
@@ -1,7 +1,13 @@
 #ifndef CAN_H
 #define CAN_H
 
-#include <linux/can.h>
+#include <stdint.h>
+
+typedef struct can_frame_t {
+    int32_t id;
+    int8_t len;
+    uint8_t data[8];
+} can_frame_t;
 
 // Open a CAN socket
 int open_can_socket(const char *ifname);
@@ -10,7 +16,7 @@ int open_can_socket(const char *ifname);
 int write_can_frame(int sock, int can_id, const unsigned char *data, int data_length);
 
 // Read a CAN frame from the socket
-int read_can_frame(int sock, struct can_frame *frame);
+int read_can_frame(int sock, struct can_frame_t *frame);
 
 // Close the CAN socket
 void close_can_socket(int sock);

62013cb0aa9ccd5910b2c7079737ec7169ba6968
david howard <dmh2000@gmail.com>
2024-08-07 16:22:07 -0700

    feat: add basic CAN frame and socket functions

--- can/can.h
+++ can/can.h
@@ -1,11 +1,17 @@
 #ifndef CAN_H
 #define CAN_H
 
-#include <linux/can.h>
+#include <stdint.h>
+
+typedef struct can_frame_t {
+    uint8_t id;
+    uint8_t len;
+    uint8_t data[8];
+} can_frame_t;
 
 int open_can_socket(const char *interface);
-int write_can_socket(int sock, struct can_frame *frame);
-int read_can_socket(int sock, struct can_frame *frame);
+int write_can_socket(int sock, struct can_frame_t *frame);
+int read_can_socket(int sock, struct can_frame_t *frame);
 int close_can_socket(int sock);
 
 #endif  // CAN_H
